7316757860b52e65fedceb02bdba35a7b5c65828
1nnovator <1nnovator1105@gmail.com>
2024-09-28 02:05:13 +0900

    fix: og2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import { ReactQueryClientProvider } from "@/components/providers/ReactQueryClientProvider";
 import Wrapper from "@/components/share/Wrapper";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const defaultUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 export const metadata = {
