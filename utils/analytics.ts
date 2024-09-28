055ae1c55a369f46c1cee34df0bef75be98023f6
1nnovator <1nnovator1105@gmail.com>
2024-09-29 06:06:07 +0900

    feat: amplitude defaultTracking true for warning

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,7 +1,9 @@
 import * as amplitude from "@amplitude/analytics-browser";
 
 export function init() {
-  amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY as string);
+  amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY as string, {
+    defaultTracking: true,
+  });
 }
 
 export function logEvent(event: string, properties?: Record<string, any>) {
