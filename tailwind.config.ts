4aed454cd65190c5eb7575b8bd3253fb66b52220
Kim Min Sung <1nnovator1105@gmail.com>
2024-09-29 16:50:11 +0900

    fix: force lightmode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,6 +99,9 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate"), require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 } satisfies Config;
 
 export default config;
